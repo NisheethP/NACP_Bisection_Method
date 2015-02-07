f5fd19b0f99487df57c5bec6e41453aed7de4ba8
Nisheeth <pandey.nisheeth59916031@gmail.com>
2015-02-07 21:40:27 +0530

    Dumped full handling of all functions. Implemented solutions for real roots of polynomials

--- NACP_Bisection/Main.cpp
+++ NACP_Bisection/Main.cpp
@@ -2,9 +2,36 @@
 #include "Functions.h"
 #include "Equation.h"
 #include <iostream>
+#include "Polynomial.h"
+
 
 int main()
 {
+	SetDefaultColour();
+
+	int degree = 0;
+	doubleVector coefs;
+
+	std::cout << "Enter degree of the equation: ";
+	std::cin >> degree;
+	std::cout << std::endl << "Enter the coefficient of - ";
+	for (int i = 0; i < degree+1; i++)
+	{
+		float coef;
+		std::cout << "\n\t\tx raised to power of " << i << ": ";
+		std::cin >> coef;
+
+		coefs.push_back(coef);
+	}
+
+	Polynomial eqn(degree);
+
+	eqn.setCoeffs(coefs);
+
+	eqn.root_Bisection();
+
+	std::cout << "\n\nRoot = " << eqn.getBestRoot();
+	
 	PressAnyKey();
 	return 0;
 }
